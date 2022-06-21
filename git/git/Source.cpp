6aa888936d4d7a432a0bd664fe90b34f55ede2cd
dtroja71 <dtroja71@gmail.com>
2022-06-21 20:20:30 +0300

    add functions

--- git/git/Source.cpp
+++ git/git/Source.cpp
@@ -2,13 +2,27 @@
 #include <string>
 
 using namespace std;
+using si_t = short int;
+typedef unsigned long long l_t;
+unsigned long long num1 = 10l;
+l_t num2 = 100l;
 
+int sum(int a, int b) {
+	return a + b;
+}
 
+float sum(float a, float b) {
+	return a + b;
+}
+
+l_t sum(l_t a, l_t b) {
+	return a + b;
+}
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-
+	
 	system("pause");
 	return 0;
 }
